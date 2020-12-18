0c47867f31ce97fba8c34381c4a0e5fed57a2c1b
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-18 12:17:24 -0500

    about and footer components

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is about us...</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nam quos, distinctio labore deserunt quibusdam atque sunt ducimus dolor ullam voluptatum non velit magni molestias, similique dolores provident consequuntur sequi.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer bg-dark">
+        <div className="container text-center">
+          <span className="text-muted">@2020 Built by Thomas Gilbert</span>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
