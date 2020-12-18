af997e92a8abdc3e65da9e41dad1296deab24c41
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-18 15:14:18 -0500

    create a private route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, { useEffect, useState, Component} from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import jwt_decode from 'jwt-decode'
 import Welcome from './components/Welcome'
+import Navbar from './components/Navbar'
 import './App.css';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = localStorage.getItem('jwtToken')
+  return  <Route {...rest} render={(props) => {
+    return user ? <Component {...rest} {...props}/>
+    :
+    <Redirect to="/login"/>
+  }}/>
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
+
       <Welcome />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,26 @@ const Navbar = () => {
               <NavLink className="nav-link" exact to="/about">About</NavLink>
             </li>
           </ul>
-          
+          {
+            props.isAuth ?
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+              </li>
+              <li className="nav-item">
+                <span onClick={props.handleLogout} className="nav-link logout-link">Logout</span>
+              </li>
+            </ul> 
+          :
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">Signup</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Login</NavLink>
+              </li>
+            </ul> 
+          }
         </div>
 
       </div>
